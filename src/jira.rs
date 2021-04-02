efeffd574418c8848916654377149e4a1304e2f7
Mason Edgel <medgel@reliaquest.com>
2021-04-02 13:22:18 -0600

    JIRA integration and basic functionality

---
+++ src/jira.rs
@@ -0,0 +1,149 @@
+use log::{debug, warn};
+use reqwest::{Client, Response};
+use serde::{Deserialize, Serialize};
+
+/// Jira instance connection information
+pub struct JiraConnection {
+    pub user: String,
+    pub token: String,
+    pub instance_url: String,
+}
+
+/// A structure representing a Jira work log.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct JiraWorklog {
+    pub started: String,
+    pub time_spent_seconds: i64,
+}
+
+/// Generic get function for Jira API
+async fn get(
+    rc: &Client,
+    jc: &JiraConnection,
+    endpoint: &str,
+    query: &Vec<(String, String)>,
+) -> reqwest::Result<Response> {
+    rc.get(format!(
+        "{base_url}/{endpoint}",
+        base_url = jc.instance_url,
+        endpoint = endpoint
+    ))
+    .basic_auth(&jc.user, Some(&jc.token))
+    .header("Accept", "application/json")
+    .query(query)
+    .send()
+    .await
+}
+
+/// Generic post function for Jira API
+async fn post(
+    rc: &Client,
+    jc: &JiraConnection,
+    endpoint: &str,
+    body: String,
+) -> reqwest::Result<Response> {
+    rc.post(format!(
+        "{base_url}/{endpoint}",
+        base_url = jc.instance_url,
+        endpoint = endpoint
+    ))
+    .basic_auth(&jc.user, Some(&jc.token))
+    .header("Accept", "application/json")
+    .header("Content-Type", "application/json")
+    .body(body)
+    .send()
+    .await
+}
+
+#[derive(Serialize, Deserialize)]
+struct JiraResponseWorklog {
+    worklogs: Vec<JiraWorklog>,
+}
+
+/// Pulls existing worklogs from Jira for a given issue.
+pub async fn get_worklogs(rc: &Client, jc: &JiraConnection, issue: &str) -> Vec<JiraWorklog> {
+    // Fetch worklogs
+    match get(
+        rc,
+        jc,
+        &format!("rest/api/3/issue/{issue}/worklog", issue = issue),
+        &vec![],
+    )
+    .await
+    {
+        // Handle failed connections
+        Err(e) => {
+            warn!(
+                "Connection error fetching worklogs for {issue}: {error}",
+                issue = issue,
+                error = e
+            );
+            vec![]
+        }
+        // On successful connection, ensure success
+        Ok(r) => match r.status().is_success() {
+            false => {
+                warn!(
+                    "Error fetching worklogs for {issue}: {status}",
+                    issue = issue,
+                    status = r.status()
+                );
+                vec![]
+            }
+            // On successful fetch, move on
+            true => {
+                let body = &r.text().await.unwrap().clone();
+                let jr: JiraResponseWorklog = match serde_json::from_str(&body) {
+                    Err(e) => {
+                        warn!(
+                            "Error parsing worklogs for {issue}: {error}",
+                            issue = issue,
+                            error = e
+                        );
+                        debug!("Body: {body}", body = &body);
+                        JiraResponseWorklog { worklogs: vec![] }
+                    }
+                    Ok(r) => r,
+                };
+                jr.worklogs
+            }
+        },
+    }
+}
+
+/// Uploads a worklog to Jira.
+pub async fn upload_worklog(
+    rc: &Client,
+    jc: &JiraConnection,
+    issue: &str,
+    wl: &JiraWorklog,
+) -> Result<(), String> {
+    // Fetch worklogs
+    match post(
+        rc,
+        jc,
+        &format!("rest/api/3/issue/{issue}/worklog", issue = issue),
+        serde_json::to_string(&wl).unwrap(),
+    )
+    .await
+    {
+        // Handle failed connections
+        Err(e) => Err(format!(
+            "Connection error uploading worklog for {issue}: {error}",
+            issue = issue,
+            error = e
+        )),
+        // On successful connection, check status code
+        Ok(r) => match r.status().is_success() {
+            false => Err(format!(
+                "Error submitting worklog for {issue}: {status}\n{body:?}",
+                issue = issue,
+                status = r.status(),
+                body = r.text().await.unwrap()
+            )),
+            // On successful fetch, move on
+            true => Ok(()),
+        },
+    }
+}
